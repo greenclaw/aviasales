67ec1ef3002e93f69fa6b11b7c4ac52353acd718
decaster3 <rinat777hatipov@gmail.com>
2016-07-20 22:47:23 +0300

    Create validation of dates

--- src/com/company/FormNum1.java
+++ src/com/company/FormNum1.java
@@ -4,11 +4,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by rinathatipov on 17.07.16.
  */
 public class FormNum1 extends JFrame {
+
+    public static final Pattern pattern = Pattern.compile
+            ("(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)");
+
+    public static boolean doMatch(String word) {
+        Matcher matcher = pattern.matcher(word);
+        return matcher.matches();
+    }
+
+
     JButton clearConfermation, conferm;
     JLabel dateofDeparture, dateofArrival, cityFrom, cityTo;
     JTextField dateofDepartureTextField, dateofArrivalTextField, cityFromTextField, cityToTextField;
@@ -36,7 +48,7 @@ public class FormNum1 extends JFrame {
         form1.add(clearConfermation);
         form1.add(conferm);
         form1.add(dateofDeparture);
-        form1. add(dateofDepartureTextField);
+        form1.add(dateofDepartureTextField);
         form1.add(dateofArrival);
         form1.add(dateofArrivalTextField);
         form1.add(cityFrom);
@@ -51,16 +63,23 @@ public class FormNum1 extends JFrame {
 
     class InformationFromFormOne implements ActionListener{
             public void actionPerformed(ActionEvent e) {
-
                 if (e.getSource() == conferm){
                     String dateOfDeparture = (dateofDepartureTextField.getText());
                     String dateOfArrival   = (dateofArrivalTextField.getText());
                     String cityFrom   = (cityFromTextField.getText());
                     String cityTo  = (cityToTextField.getText());
                     suggestion = new Suggestion(dateOfDeparture, dateOfArrival, true, cityFrom, cityTo);
-                    form1.setVisible(false);
-                    FormNum2 form2 = new FormNum2();
+                    if(doMatch(dateOfArrival) ){
+                        if(doMatch(dateOfDeparture)) {
+                            form1.setVisible(false);
+                            FormNum2 form2 = new FormNum2();
+                        }
+                        else{JOptionPane.showMessageDialog(form1, "Date must be in format **/**/****");}
+                    }
+                    else{JOptionPane.showMessageDialog(form1, "Date must be in format **/**/****");
+                    }
                 }
+
 
                 if (e.getSource() == clearConfermation){
                     dateofArrivalTextField.setText(null);
