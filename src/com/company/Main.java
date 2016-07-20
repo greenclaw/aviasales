db534d5790127b90265c6c90a51c73b34f8a93e9
varnavarb <romavarnava@gmail.com>
2016-07-21 02:38:37 +0300

    Modifing database, fullfill table ticket, flight

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,17 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.Normalizer;
+import java.util.Random;
 
 public class Main extends Thread{
-    public static void main(String[] args) {
-        FormNum1 form1 = new FormNum1();
+	
+    public static void main(String[] args) throws SQLException {
+//        FormNum1 form1 = new FormNum1();
+    	DataBase.getConnection();
+    	
+		
     }
 }
